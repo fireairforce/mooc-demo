87ccb864b76a9e08ff3f09b05f552eca1fc69e70
zoomdong <1344492820@qq.com>
2018-08-19 09:35:43 +0800

    完整的整个demo

---
+++ src/CommentApp.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react'
+import CommentInput from './CommentInput'
+import CommentList from './CommentList'
+import './index.css'
+
+class CommentApp extends Component
+{
+    constructor(){
+        super()
+        this.state={
+            comments:[]
+        }
+    }
+    handleSubmitComment(comment){
+       this.state.comments.push(comment)
+       this.setState({
+           comments:this.state.comments
+       })
+    }
+    render(){
+        return(
+            <div className="wrapper">
+                <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
+                <CommentList comments={this.state.comments} />
+            </div>    
+        )
+    }
+}
+
+export default CommentApp
